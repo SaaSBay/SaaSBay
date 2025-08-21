15f256b10763d8fb534734644c2c2617a50ab75a
shaikmohammed-maaz <shaikmaaz333@gmail.com>
2025-08-21 11:14:16 +0530

    changed the base to /

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/SaaSBay/',
+  base: '/',
   plugins: [react(), tailwindcss(),],
   
 })
